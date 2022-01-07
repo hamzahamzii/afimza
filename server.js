b421b46775c317da3a50173c3f0baad711ae7a8b
hmzaehmad <hamza.ahmad@keeptruckin.com>
2022-01-07 21:10:57 +0500

    build added

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
+// LIB IMPORTS
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-// Models
+// MODELS
 const Thought = require("./models/thought");
 
+// GLOBAL VARS
+const PORT = process.env.PORT || 5000;
 const dbURI =
+  process.env.MONGODB_URI ||
   "mongodb+srv://hamzahamzii:Blah-12345@afimza.loxun.mongodb.net/afimza?retryWrites=true&w=majority";
 
+// APP INITIALIZATION PLUS CONFIGURING MIDDLEWARES
 const app = express();
 app.use(cors());
 app.use(
@@ -18,15 +23,13 @@ app.use(
 );
 app.use(bodyParser.json());
 
+// CONNECTING TO DB
 mongoose
   .connect(dbURI)
   .then(() => console.log("connection successfull"))
   .catch((err) => console.log(err));
 
-app.listen(8000, "localhost", () => {
-  console.log("started listening");
-});
-
+// ROUTES
 app.post("/thoughts", (req, res) => {
   console.log("got req", req.body);
   const thought = new Thought(req.body);
@@ -34,7 +37,6 @@ app.post("/thoughts", (req, res) => {
   console.log("thought saved", thought);
   res.end(JSON.stringify(thought));
 });
-
 app.get("/thoughts", (req, res) => {
   console.log("get thoughts", req.body);
   Thought.find()
@@ -44,10 +46,19 @@ app.get("/thoughts", (req, res) => {
     })
     .catch((err) => console.log(err));
 });
-
 app.delete("/thoughts/:id", (req, res) => {
   const id = req.params.id;
   console.log(id);
   Thought.findByIdAndDelete(req.params.id).then((res) => console.log(res));
   res.end("del done");
+});
+
+// PRODUCTION CHECKS
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// STARTING SERVER
+app.listen(PORT, "localhost", () => {
+  console.log("started listening");
 });
